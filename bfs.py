62cbd9dda19fa5e1a513375d536d957acc418ab2
Lord Charité Igirimbabazi <62570559+Lord-Charite@users.noreply.github.com>
2023-12-27 14:50:19 -0500

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -11,27 +11,30 @@ from collections import deque
 # BFS
 
 def breadth_first_search(start, goal):
-    frontier = deque()
-    back_pointer = {}
-    frontier.append(start)
-    back_pointer[start] = None
+    frontier = deque([start])
+    back_pointer = { start : None}
     while len(frontier) > 0:
         v = frontier.popleft()
         for neighbour in v.adj_list_:
+            
+            if neighbour == goal.name:
+                break
+                
             if neighbour not in back_pointer:
                 frontier.append(neighbour)
                 back_pointer[neighbour] = v
-            if neighbour == goal.name:
-                break
 
-    vertex = goal
-
-    path = []
-
-    while (vertex != None):
-        path.append(vertex)
-        vertex = back_pointer[vertex]
+    path = reconstruct_path(goal, back_pointer)
     return path
 
+def reconstruct_path(goal, back_pointer):
+    path = []
+    current_vertex = goal
+
+    while current_vertex is not None:
+        path.append(current_vertex)
+        current_vertex = back_pointer[current_vertex]
+
+    return path[::-1]  # Reverse the path to start from the 'start' vertex
 
 
