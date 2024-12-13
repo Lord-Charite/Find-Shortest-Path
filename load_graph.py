51da514941523c913c9a507f5fdd8cf7a73c0ee0
Charité Igirimbabazi <62570559+lord-charite@users.noreply.github.com>
2024-12-13 13:41:18 -0500

    Update load_graph.py

--- load_graph.py
+++ load_graph.py
@@ -12,17 +12,19 @@ from vertex import Vertex
 
 
 def load_graph(filename):
-    vertex_dict = {}
+    vertex_dict = {} # individual vertices and their location
     fp = open(filename, "r")
     for line in fp:
-        field_list= line.split(";")
+        field_list= line.split(";") # split <vertex_name>;<adjacent_vertices>;<location>
+        
         for i in range(len(field_list)):
-            field_list[i] = field_list[i].strip()
+            field_list[i] = field_list[i].strip() 
 
-        location= field_list[2].split(",")
-        vertex_= field_list[0]
-        v = Vertex(vertex_, location[0], location[1])
-        vertex_dict[field_list[0]] = v
+        location= field_list[2].split(",") #location split into latitude and longitude
+        vertex_= field_list[0] # get starting vertex
+        
+        v = Vertex(vertex_, location[0], location[1]) # create vertex object
+        vertex_dict[field_list[0]] = v # and add to dictionary
 
     fp.close()
 
@@ -31,14 +33,21 @@ def load_graph(filename):
         field_list = line.split(";")
         for i in range(len(field_list)):
             field_list[i] = field_list[i].strip()
+
+        #get neigbors
         adj_vertex = field_list[1]
         adj_list = adj_vertex.split(",")
+        
         for i in range(len(adj_list)):
             adj_list[i] = adj_list[i].strip()
+            
         vertex_ = field_list[0]
         v = vertex_dict[vertex_]
+        
         for v_name in adj_list:
-            v_obj = vertex_dict[v_name]
-            v.adj_list_.append(v_obj)
+            v_obj = vertex_dict[v_name] # retrieve the corresponding Vertex object for the adjacent vertex
+            v.adj_list_.append(v_obj) # append the adjacent Vertex object to the current vertex's adjacency list
+            
     fp1.close()
+    
     return vertex_dict
